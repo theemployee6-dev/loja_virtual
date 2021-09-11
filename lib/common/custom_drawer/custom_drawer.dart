97ad6f63ddf968a05ba265727303ecd3afe64b63
willians tavares <theemployee6@gmail.com>
2021-09-11 18:08:34 -0300

    configurando o drawer do app
    personalizando o drawer do app

--- lib/common/custom_drawer/custom_drawer.dart
+++ lib/common/custom_drawer/custom_drawer.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Drawer(
+      child: ListView(
+        children: [
+          
+        ],
+      ),
+    );
   }
 }
