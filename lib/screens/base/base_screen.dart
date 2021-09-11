ed56446e475958cf1206712e381a8c9bec8996ee
willians tavares <theemployee6@gmail.com>
2021-09-11 17:40:30 -0300

    pequena alteração na main e na baseScreen

--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
 class BaseScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return PageView(
+      children: [
+        Container(color: Colors.red),
+        Container(color: Colors.yellow),
+        Container(color: Colors.green),
+      ],
     );
   }
 }
