8c8ac97425b51f92e9322a46870e3571535bace3
willians tavares <theemployee6@gmail.com>
2021-09-11 17:23:56 -0300

    update main page
    retirei a faixa padrão debug

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Loja do Will',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
