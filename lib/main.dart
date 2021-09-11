70e66c42e7dbc16342b3be82ed76d9a942971b92
willians tavares <theemployee6@gmail.com>
2021-09-11 17:27:32 -0300

    criando a base_screen do app
    base screen responsável por conter todas as outras páginas e navegar entre elas

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'screens/base/base_screen.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -13,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: BaseScreen(),
     );
   }
 }
