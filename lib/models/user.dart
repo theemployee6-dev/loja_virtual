476dad395dce1d3dd6ed03ca3a9a0b10c47cfbd9
willians tavares <theemployee6@gmail.com>
2021-09-11 22:02:12 -0300

    Update user.dart

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,19 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
 class User {
-  String id;
-  String name;
+
+  User({this.email, this.password});
+
   String email;
   String password;
-  String confirmPassword;
 
-  bool admin = false;
-
-  User({this.email, this.password, this.name, this.id});
-
-  User.fromDocument(DocumentSnapshot document) {
-    id = document.documentID;
-    name = document.data['name'] as String;
-    email = document.data['email'] as String;
-  }
 }
