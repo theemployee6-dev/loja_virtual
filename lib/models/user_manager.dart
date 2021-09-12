6dddd04e80568e865bdfdc52789e8d0801875ee0
willians tavares <theemployee6@gmail.com>
2021-09-12 15:13:34 -0300

    updated drawer, usermanager e loginscreen

--- lib/models/user_manager.dart
+++ lib/models/user_manager.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
@@ -7,26 +8,47 @@ import '/models/user.dart';
 import 'user.dart';
 
 class UserManager extends ChangeNotifier {
+  UserManager() {
+    _loadCurrentUser();
+  }
+
   final FirebaseAuth auth = FirebaseAuth.instance;
+  final Firestore firestore = Firestore.instance;
 
-  bool loading = false;
+  FirebaseUser user;
 
-  void setLoading(bool value) {
-    loading = value;
+  bool _loading = false;
+  bool get loading => _loading;
+
+  set loading(bool value) {
+    _loading = value;
+    notifyListeners();
+  }
+
+  bool get isLoggedIn => user != null;
+
+  Future<void> _loadCurrentUser() async {
+    final FirebaseUser currentUser = await auth.currentUser();
+    if (currentUser != null) {
+      user = currentUser;
+    }
     notifyListeners();
   }
 
   Future<void> signIn({User user, Function onFail, Function onSuccess}) async {
-    setLoading(true);
+    loading = true;
     try {
       final AuthResult result = await auth.signInWithEmailAndPassword(
           email: user.email, password: user.password);
+
+      this.user = result.user;
 
       print(result.user.uid);
       onSuccess();
     } on PlatformException catch (e) {
       print(e);
       onFail(getErrorString(e.code));
+      loading = false;
     }
   }
 }
