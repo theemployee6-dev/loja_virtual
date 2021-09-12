4ffa620b3cbaa90d160f1075077cfa23d7d1f722
willians tavares <theemployee6@gmail.com>
2021-09-12 19:57:11 -0300

    update user.dart, base_screen.dart e signup_screen.dart

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,6 +1,8 @@
 class User {
-  User({this.email, this.password});
+  User({this.email, this.password, this.name});
 
+  String name;
   String email;
   String password;
+  String confirmPassword;
 }
