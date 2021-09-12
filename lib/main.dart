f66b6ced6eb924a1eb6dccca006f8ef63d9bf2eb
willians tavares <theemployee6@gmail.com>
2021-09-11 22:00:54 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'models/user_manager.dart';
 import 'screens/base/base_screen.dart';
 
 void main() {
@@ -10,15 +12,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Loja do Willians',
-      theme: ThemeData(
-        primaryColor: const Color.fromARGB(255, 4, 125, 141),
-        scaffoldBackgroundColor: const Color.fromARGB(255, 4, 125, 141),
-        appBarTheme: const AppBarTheme(elevation: 0),
+    return Provider(
+      create: (_) => UserManager(),
+      child: MaterialApp(
+        title: 'Loja do Daniel',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: const Color.fromARGB(255, 4, 125, 141),
+          scaffoldBackgroundColor: const Color.fromARGB(255, 4, 125, 141),
+          appBarTheme: const AppBarTheme(elevation: 0),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: BaseScreen(),
       ),
-      home: BaseScreen(),
     );
   }
 }
