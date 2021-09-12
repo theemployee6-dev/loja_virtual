1a86f6954ebcceab07fd807ec856a812b8e0026e
willians tavares <theemployee6@gmail.com>
2021-09-12 20:48:43 -0300

    Update user.dart

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,8 +1,25 @@
-class User {
-  User({this.email, this.password, this.name});
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class User {
+  User({this.email, this.password, this.name, this.id});
+
+  String id;
   String name;
   String email;
   String password;
   String confirmPassword;
+
+  DocumentReference get firestoreRef =>
+      Firestore.instance.document('users/$id');
+
+  Future<void> saveData() async {
+    await firestoreRef.setData(toMap());
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'email': email,
+    };
+  }
 }
