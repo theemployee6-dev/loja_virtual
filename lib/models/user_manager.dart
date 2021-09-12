0075fc0b198bb38ce40c5d863703a08101683169
willians tavares <theemployee6@gmail.com>
2021-09-11 22:16:18 -0300

    update main.dart e user_manager.dart

--- lib/models/user_manager.dart
+++ lib/models/user_manager.dart
@@ -1,14 +1,23 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 import 'package:loja_virtual/helpers/firebase_errors.dart';
 import '/models/user.dart';
 
 import 'user.dart';
 
-class UserManager {
+class UserManager extends ChangeNotifier {
   final FirebaseAuth auth = FirebaseAuth.instance;
 
+  bool loading = false;
+
+  void setLoading(bool value) {
+    loading = value;
+    notifyListeners();
+  }
+
   Future<void> signIn({User user, Function onFail, Function onSuccess}) async {
+    setLoading(true);
     try {
       final AuthResult result = await auth.signInWithEmailAndPassword(
           email: user.email, password: user.password);
