6d17754276ecc97517fa6e417bd5616ef20da96a
willians tavares <theemployee6@gmail.com>
2021-09-26 22:44:04 -0300

    update main, cart_manager, cart_product, user, cart_screen

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -3,7 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class User {
   User({this.email, this.password, this.name, this.id});
 
-  User.fromDocument(DocumentSnapshot document){
+  User.fromDocument(DocumentSnapshot document) {
     id = document.documentID;
     name = document.data['name'] as String;
     email = document.data['email'] as String;
@@ -17,6 +17,8 @@ class User {
 
   DocumentReference get firestoreRef =>
       Firestore.instance.document('users/$id');
+
+  CollectionReference get cartReference => firestoreRef.collection('cart');
 
   Future<void> saveData() async {
     await firestoreRef.setData(toMap());
